515cdd730f18f3bac4605b270a693ee7160d157c
Nate Gay <email@nategay.me>
2026-07-07 01:44:28 -0500

    Get flakes working on Mac

--- dot_config/user-packages/flake.nix
+++ dot_config/user-packages/flake.nix
@@ -5,7 +5,7 @@
 
   outputs = { self, nixpkgs }:
     let
-      systems = [ "x86_64-linux" "aarch64-linux" ];
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ];
       forEachSystem = f:
         nixpkgs.lib.genAttrs systems (system:
           f (import nixpkgs {
