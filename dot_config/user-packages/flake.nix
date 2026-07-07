c6fe564adbbcc8a6d59124a2a2884c873a71ec9f
Nate Gay <email@n8.gay>
2026-07-07 18:00:22 -0500

    Add warp to user-packages

--- dot_config/user-packages/flake.nix
+++ dot_config/user-packages/flake.nix
@@ -30,6 +30,7 @@
             pkgs.gnupg # GNU Privacy Guard (GPL-3)
             pkgs.helix # Post-modern modal text editor, provides `hx` (MPL-2)
             pkgs.starship # Cross-shell prompt (ISC, Rust)
+            pkgs.warp-terminal # Rust-based terminal, provides `warp-terminal` (AGPL-3, MIT)
             pkgs.zoxide # Smarter cd command (MIT, Rust)
 
             # Passphrase entry dialog for GnuPG
