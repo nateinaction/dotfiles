91a5bb9f7fe78f349b7a4c1bdb6aba1401c2d523
Nate Gay <nate.gay@wpengine.com>
2026-07-10 13:26:34 -0500

    Set zellij socket dir globally so session name length constraint is loosened

--- dot_config/private_fish/conf.d/code.function.fish
+++ dot_config/private_fish/conf.d/code.function.fish
@@ -7,10 +7,6 @@ function code
     set -l path (cd "$argv[1]" 2>/dev/null && pwd || echo "$argv[1]")
     set -l session_name "code-"(basename "$path")
 
-    # Nix sets a long $TMPDIR that pushes zellij's Unix socket path
-    # past the 103-byte kernel limit.
-    set -lx ZELLIJ_SOCKET_DIR /tmp/zellij
-
     if zellij list-sessions -s 2>/dev/null | grep -qx "$session_name"
         zellij attach "$session_name"
         return 0
